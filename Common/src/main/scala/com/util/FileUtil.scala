44a8a313d1b5c83bd67d200db7718b336a02065d
svaduka <sainagaraju.vaduka@gmail.com>
2017-09-18 17:37:22 +0530

    Updated Content

--- Common/src/main/scala/com/util/FileUtil.scala
+++ Common/src/main/scala/com/util/FileUtil.scala
@@ -4,33 +4,19 @@ package com.util
 
 import reflect.io
 import java.io.File
+import scala.io.Source
 
 object FileUtil  {
-  //Main 
-  def main(args: Array[String]): Unit = {
+  def readLines(fileName:String):List[String] = {
+    Source.fromFile(fileName).getLines().toList;    
+  }
+  
+  def readFileAbsoluteNames(directory:String):List[String] = {
     
-    //
-  // Different types of file extensions
-  val file_Extension=List("ctl","meta","docx")
-  //File Directory Path
-  val dir:String = "C:/Users/suresh/Desktop/ListDir/Meta"
-  //Method
-  def getListofFiles(dir: String,extension:List[String]):List[File] = {
-     
-   //Empty directory
- val d = new File(dir)
- //Required File Extension
- val endString ="docx"
-  // File Filtering of given File extension
- if (d.exists && d.isDirectory) {
-        d.listFiles.filter(_.isFile).toList.filter { file => file_Extension.exists(file.getName.endsWith(_))}
-    } else {
-        List[File]()
-    }
-}
-//Call the Method
-  val files = getListofFiles(dir, file_Extension)
-files.foreach(println)
- 
-}
+//    Source.fr
+    
+    ???
+  }
+  
+  
 }
