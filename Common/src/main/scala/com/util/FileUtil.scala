a0c7d394d36deae8520282347c72c6a6145ad1c6
svaduka <sainagaraju.vaduka@gmail.com>
2017-09-18 18:07:45 +0530

    Created List Files reading directories

--- Common/src/main/scala/com/util/FileUtil.scala
+++ Common/src/main/scala/com/util/FileUtil.scala
@@ -1,22 +1,43 @@
 package com.util
 
-
-
 import reflect.io
 import java.io.File
 import scala.io.Source
+import java.io.FilenameFilter
 
 object FileUtil  {
   def readLines(fileName:String):List[String] = {
     Source.fromFile(fileName).getLines().toList;    
   }
   
-  def readFileAbsoluteNames(directory:String):List[String] = {
+  private def recursiveListFiles(f: File): Array[File] = {
+  val these = f.listFiles
+  these ++ these.filter(_.isDirectory).flatMap(recursiveListFiles(_))
+}
+  
+  def recursiveListFiles(directoryName: String): Array[String] = {
     
-//    Source.fr
-    
+  val input = new File(directoryName);
+  
+  if(input.isDirectory())
+  {
+  recursiveListFiles(input).map(f=> f.getAbsolutePath());
+  }else{
     ???
   }
   
+}
+  
+  def getListOfFiles(dir: File, extensions: List[String]): List[File] = {
+    dir.listFiles.filter(_.isFile).toList.filter { file =>
+        extensions.exists(file.getName.endsWith(_))
+    }
+}
+  
+  def getListOfFiles(dir: File, extensions: String): List[File] = {
+    dir.listFiles.filter(_.isFile).toList.filter { file =>
+        file.getName.endsWith(extensions)
+    }
+}
   
 }
