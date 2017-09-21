c9356f32f2bc3ca498bbc8e9b0d34b9fcad01509
srivani <srivani@srivani-PC>
2017-09-21 10:59:23 +0530

    Daily Activity scrum call done

--- Common/src/main/scala/com/util/RDBMSConnection.scala
+++ Common/src/main/scala/com/util/RDBMSConnection.scala
@@ -4,6 +4,7 @@ import java.sql.DriverManager
 import java.sql.PreparedStatement
 import java.sql.ResultSet
 import java.sql.Statement
+import com.FileUtils.PropertyReader
 
 object RDBMSConnection {
   
